d5dc2f9287eba8ede0f8233258885a42662e34b5
anautonomousagent <anautonomousagent@tutanota.com>
2019-06-11 19:11:44 -0700

    update nwa 6472

--- wp-content/themes/ryu/js/skip-link-focus-fix.js
+++ wp-content/themes/ryu/js/skip-link-focus-fix.js
@@ -1,1 +1,19 @@
-(function(){var b=navigator.userAgent.toLowerCase().indexOf("webkit")>-1,c=navigator.userAgent.toLowerCase().indexOf("opera")>-1,a=navigator.userAgent.toLowerCase().indexOf("msie")>-1;if((b||c||a)&&"undefined"!==typeof(document.getElementById)){var d=(window.addEventListener)?"addEventListener":"attachEvent";window[d]("hashchange",function(){var e=document.getElementById(location.hash.substring(1));if(e){if(!/^(?:a|select|input|button|textarea)$/i.test(e.tagName)){e.tabIndex=-1}e.focus()}},false)}})();
+( function() {
+	var is_webkit = navigator.userAgent.toLowerCase().indexOf( 'webkit' ) > -1,
+	    is_opera  = navigator.userAgent.toLowerCase().indexOf( 'opera' )  > -1,
+	    is_ie     = navigator.userAgent.toLowerCase().indexOf( 'msie' )   > -1;
+
+	if ( ( is_webkit || is_opera || is_ie ) && 'undefined' !== typeof( document.getElementById ) ) {
+		var eventMethod = ( window.addEventListener ) ? 'addEventListener' : 'attachEvent';
+		window[ eventMethod ]( 'hashchange', function() {
+			var element = document.getElementById( location.hash.substring( 1 ) );
+
+			if ( element ) {
+				if ( ! /^(?:a|select|input|button|textarea)$/i.test( element.tagName ) )
+					element.tabIndex = -1;
+
+				element.focus();
+			}
+		}, false );
+	}
+})();
